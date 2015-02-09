3ac9921161530bb1918f174c7a7206fa87d9cc11
yujunyak <yujunyak@gmail.com>
2015-02-09 16:41:29 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,42 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myclock",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/yakeugene/myclock/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  143,
+  64,
+  155651,
+  277980,
+  645243,
+  319,
+  35607,
+  113502,
+  97570
+ ],
+ "filedates": [
+  "2015-02-09T08:39:06.464Z",
+  "2015-02-09T08:28:31.830Z",
+  "2015-02-09T08:39:33.329Z",
+  "2015-02-09T08:28:31.865Z",
+  "2015-02-09T08:28:31.894Z",
+  "2015-02-09T08:28:31.869Z",
+  "2015-02-09T08:28:31.827Z",
+  "2015-02-09T08:28:31.823Z",
+  "2015-02-09T08:39:26.288Z"
+ ],
+ "date": "2015-02-09T08:39:34.289Z"
+})
